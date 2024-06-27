6704c9651c2575cd902a459aa1b6e445ba49cb15
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-06-27 14:39:33 +0500

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavbarComponent() {
+  return (
+    <Navbar expand="lg" className="navbarComponent">
+      <Container>
+        <Navbar.Brand className='nav-brand' href="/">SheyRooms</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/register">Register</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComponent;
